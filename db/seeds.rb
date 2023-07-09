4330bc1e350deb65f1e5afd55828c2c55a973a73
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-07-10 00:05:23 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,50 @@
+users_data = [
+  {
+    name: "Alice Smith",
+    email: "alice@email.com",
+    password: "password",
+    password_confirmation: "password"
+  },
+  {
+    name: "Bob Johnson",
+    email: "bob@email.com",
+    password: "password",
+    password_confirmation: "password"
+  }
+]
 
-users =
-  User.create(
-    [
-      {
-        name: "John Doe",
-        email: "john@email.com",
-        password: "password",
-        password_confirmation: "password"
-      },
-      {
-        name: "Jane Doe",
-        email: "jane@email.com",
-        password: "password",
-        password_confirmation: "password"
-      }
-    ]
-  )
+users = User.create(users_data)
 
-categories =
-  Category.create(
-    [
-      { name: "Food", icon: "fa-solid fa-utensils" },
-      { name: "Transportation", icon: "fa-solid fa-bus" },
-      { name: "Entertainment", icon: "fa-solid fa-gamepad" },
-      { name: "Utilities", icon: "fa-solid fa-bolt" },
-      { name: "Clothing", icon: "fa-solid fa-tshirt" },
-      { name: "Health", icon: "fa-solid fa-heartbeat" },
-      { name: "Other", icon: "fa-solid fa-question" }
-    ]
-  )
+categories_data = [
+  { name: "Groceries", icon: "fa fa-cutlery" },
+  { name: "Transportation", icon: "fa fa-shopping-cart" },
+  { name: "Leisure", icon: "fa fa-credit-card" },
+  { name: "Bills", icon: "fa fa-heartbeat" },
+  { name: "Clothing", icon: "fa fa-gamepad" },
+  { name: "Healthcare", icon: "fa fa-question" },
+  { name: "Other", icon: "fa fa-medkit" }
+]
 
-expenses =
-  Expense.create(
-    [
-      { name: "Lunch", amount: 10.00, author_id: 1, category_ids: [1] },
-      { name: "Bus", amount: 2.00, author_id: 1, category_ids: [2] },
-      { name: "Movie", amount: 15.00, author_id: 1, category_ids: [3] },
-      { name: "Electricity", amount: 50.00, author_id: 1, category_ids: [4] },
-      { name: "Shirt", amount: 20.00, author_id: 1, category_ids: [5] },
-      { name: "Medicine", amount: 5.00, author_id: 1, category_ids: [6] },
-      { name: "Dinner", amount: 15.00, author_id: 1, category_ids: [1] },
-      { name: "Uber", amount: 10.00, author_id: 1, category_ids: [2] },
-      { name: "Concert", amount: 50.00, author_id: 1, category_ids: [3] },
-      { name: "Water", amount: 5.00, author_id: 1, category_ids: [4] },
-      { name: "Pants", amount: 30.00, author_id: 1, category_ids: [5] },
-      { name: "Bandage", amount: 2.00, author_id: 1, category_ids: [6] },
-      { name: "Breakfast", amount: 5.00, author_id: 1, category_ids: [1] },
-      { name: "Train", amount: 5.00, author_id: 1, category_ids: [2] },
-      { name: "Bowling", amount: 20.00, author_id: 1, category_ids: [3] },
-      { name: "Gas", amount: 30.00, author_id: 1, category_ids: [4] },
-      { name: "Shoes", amount: 50.00, author_id: 1, category_ids: [5] }
-    ]
-  )
+categories = Category.create(categories_data)
+
+expenses_data = [
+  { name: "Grocery shopping", amount: 50.00, author_id: 1, category_ids: [1] },
+  { name: "Gasoline", amount: 30.00, author_id: 1, category_ids: [2] },
+  { name: "Coffee with friends", amount: 10.00, author_id: 1, category_ids: [3] },
+  { name: "Electricity bill", amount: 100.00, author_id: 1, category_ids: [4] },
+  { name: "New shoes", amount: 75.00, author_id: 1, category_ids: [5] },
+  { name: "Prescription medication", amount: 20.00, author_id: 1, category_ids: [6] },
+  { name: "Dinner at a restaurant", amount: 50.00, author_id: 1, category_ids: [1] },
+  { name: "Taxi ride", amount: 15.00, author_id: 1, category_ids: [2] },
+  { name: "Movie tickets", amount: 25.00, author_id: 1, category_ids: [3] },
+  { name: "Water bill", amount: 50.00, author_id: 1, category_ids: [4] },
+  { name: "New jeans", amount: 60.00, author_id: 1, category_ids: [5] },
+  { name: "First aid supplies", amount: 5.00, author_id: 1, category_ids: [6] },
+  { name: "Breakfast at a cafe", amount: 15.00, author_id: 1, category_ids: [1] },
+  { name: "Train ticket", amount: 10.00, author_id: 1, category_ids: [2] },
+  { name: "Bowling alley", amount: 20.00, author_id: 1, category_ids: [3] },
+  { name: "Internet bill", amount: 80.00, author_id: 1, category_ids: [4] },
+  { name: "Sneakers", amount: 90.00, author_id: 1, category_ids: [5] }
+]
+
+expenses = Expense.create(expenses_data)
